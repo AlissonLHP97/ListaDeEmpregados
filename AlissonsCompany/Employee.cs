d615706c2258838e947e1406888556820c51d539
Alisson <alissonp-alissonp@hotmail.com>
2026-01-02 12:49:55 -0300

    Lista de Empregados v1

---
+++ AlissonsCompany/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alisson_sCompany
+{
+    internal class Employee
+    {
+        public int Id;
+        public string Name;
+        public double Salary;
+
+        public Employee ()
+        {
+            Id = Convert.ToInt32(Console.ReadLine());
+            Name = Console.ReadLine();
+            Salary = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void IncreaseSalary(double percentage)
+        {
+            Salary *= (1 + percentage / 100); 
+        }
+
+        public override string ToString() =>
+            $"""
+            Id: {Id}
+            Name: {Name}
+            Salary; {Salary:F2}
+            """;
+
+
+       
+    }
+}
---
+++ AlissonsCompany/Program.cs
@@ -0,0 +1,29 @@
+﻿using Alisson_sCompany;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace AlissonCompany;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many Employee will be registered? ");
+            int employeeRegistered = Convert.ToInt16(Console.ReadLine());
+            List<Employee> employees = new List<Employee>();
+
+        for (int i = 0; i < employeeRegistered; i++)
+            {
+             Console.WriteLine($"Employee #{i}");
+             employees.Add(new Employee());
+       
+        foreach (Employee employee in employees)
+        {
+           Console.WriteLine(employee);
+        }
+            }
+
+
+        }
+    }
